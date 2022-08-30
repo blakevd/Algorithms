c50baf92507fe3a742e6e8965a0d07cc69fb5ce0
iPupkin <blakeyvd@gmail.com>
2022-08-30 13:42:13 -0600

    working anagram finder and input reader

--- PS0.py
+++ PS0.py
@@ -1,15 +1,36 @@
 # Blake Van Dyken
 # 8/30/22
 # PS0
+from ctypes import sizeof
 import sys
+from turtle import left
 
 def main():
     numWords, numLetters = input().split(" ") # get n, k
+    words = []
     
-    print(numWords, numLetters)
+    for word in range(int(numWords)):
+        # sort the word alphabetically then add it to the list
+        words.append("".join(sorted(input())))
         
-        
-
+    print(findAnagrams(words, int(numWords)))
+    
+# sort 
+def findAnagrams(words, numWords): 
+    anagrams = 0
+    
+    for word in range(numWords): # loop through list
+        for otherWord in range(numWords): # compare to rest of list
+            if (word == otherWord): # dont compare to itself
+                break
+            
+            # check if words match and if we did not already find it
+            if (words[word] == words[otherWord]):
+                anagrams += 1
+                
+                
+    return anagrams
+    
 if __name__ == "__main__":
     main()
     
