7347abebb3e5327a8d2ba9cceedbd53a654407fc
iPupkin <blakeyvd@gmail.com>
2022-08-30 22:23:43 -0600

    rename and comments

--- PS0.py
+++ PS0.py
@@ -3,27 +3,28 @@
 # PS0
 import sys
  
-# compare sorted list of words to figure out anagrams 
-def findNonAnagrams(list, numWords): 
+# compare sorted dict of words to figure out anagrams 
+def findNonAnagrams(dict): 
     non_anagrams = 0
-    for n in list:
-        if(list[n] == 1):
+    # count occurnces that we stored in dictionary
+    for key in dict:
+        if(dict[key] == 1):
             non_anagrams += 1
             
     return non_anagrams
         
 
-# get n, k
+# get num words, num of letters
 numWords, numLetters = sys.stdin.readline().split(" ") # get n, k
-list = dict()
+dict = dict()
 
 # loop through input
 for word in range(int(numWords)):
-    # sort the word alphabetically then add it to the list
-    w = "".join(sorted(sys.stdin.readline()))
-    if (list.get(w) != None):
-        list[w] = list[w] + 1
+    # sort the word alphabetically then add it to the dict with the num of occurences
+    input = "".join(sorted(sys.stdin.readline()))
+    if (dict.get(input) != None):
+        dict[input] = dict[input] + 1
     else:
-        list[w] = 1
+        dict[input] = 1
 
-sys.stdout.write(str(findNonAnagrams(list, int(numWords)))) # print # of non anagrams
+sys.stdout.write(str(findNonAnagrams(dict))) # print # of non anagrams
