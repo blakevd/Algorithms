db2a38596a814e1f8b40f8781661b5361f08652d
iPupkin <blakeyvd@gmail.com>
2022-08-30 18:06:31 -0600

    fixed anagram func

--- PS0.py
+++ PS0.py
@@ -5,21 +5,22 @@ import sys
  
 # compare sorted list of words to figure out anagrams 
 def findNonAnagrams(words, numWords): 
-    not_anagrams = 0
-    
+    anagrams = 0
     for word in range(numWords - 1): # loop through list
-        for otherWord in range(word, numWords - 1): # loop word against other words
-            if (words[word] != words[otherWord]): # check if next item is anagram
-                not_anagrams += 1    
+        for otherWord in range(word+1, numWords): # loop word against other words
+            if (words[word] == words[otherWord]): # check if next item is anagram
+                anagrams += 1
 
-    return not_anagrams
+    # multiply anagrams by 2 because each anagram is a pair
+    return numWords - (anagrams*2) # return total non anagrams
 
 # get n, k
 numWords, numLetters = sys.stdin.readline().split(" ") # get n, k
 words = []
 
+# loop through input
 for word in range(int(numWords)):
     # sort the word alphabetically then add it to the list
     words.append("".join(sorted(sys.stdin.readline()))) # get word from stdin
-print(words)
+
 sys.stdout.write(str(findNonAnagrams(words, int(numWords)))) # print # of non anagrams
