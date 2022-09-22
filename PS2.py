0995732c4b1f9b3a4f1b70aac1a00485bc26babf
iPupkin <blakeyvd@gmail.com>
2022-09-22 15:10:19 -0600

    method almost works im 10 #s off

--- PS2.py
+++ PS2.py
@@ -11,24 +11,35 @@ def climb(totalHeight, n, memory, distances):
                     memory[h][i] = 0
             else: # fill in between columns and last col
                 nextDist = distances[i-1] # next distance we need to go up and down
+
                 # set values to where they are in mem unless they are out of bounds
-                up, down = NINF, NINF # takes care of out of bounds case
+                if(h - nextDist >= 0): # will ignore out of bounds case
+                    down = memory[h - nextDist][i - 1]
+                    if(down is not NINF): # ignore if it is inf
+                        if(down >= h):
+                            memory[h][i] = down
+                            print("1chose: ",down,"over: ",h)
+                        else:
+                            memory[h][i] = h
+                            print("2chose: ",h,"over: ",down)
                 if(h + nextDist <= totalHeight):
                     up = memory[h + nextDist][i - 1]
-                if(h - nextDist >= 0):
-                    down = memory[h - nextDist][i - 1]
+                    if(up is NINF): # ignores it if it is inf
+                        print("3chose: ",max(h, memory[h][i]),"over: ",min(h, memory[h][i]))
+                        memory[h][i] = max(h, memory[h][i]) # chooses between what down was stored in here or h
+                    else:
+                        if(memory[h][i] is not NINF):
+                            print("4chose: ",min(up, memory[h][i]),"over: ",max(up, memory[h][i]))
+                            memory[h][i] = min(up, memory[h][i]) # choose min between itsekf and down 
+                        else:
+                            print("4chose: ",up,"over: ",memory[h][i])
+                            memory[h][i] = max(h, up)
                 
-                if(down > up):
-                    memory[h][i] = h
-                elif(down < up and down is not NINF):
-                    memory[h][i] = down
-                elif(down < up and down is NINF):
-                    memory[h][i] = up
-                else:
-                    memory[h][i] = NINF # ???
-
+            #if(i == n-2):
+                #print(memory[h][i])
             if(i == n-1 and memory[h][i] is not NINF):
                 result = memory[h][i]
+                #print(h, i, "mem: ", memory[h][i])
                 if (result != NINF and h - distances[i] == 0): # i = n - 1 is not INF
                     return result
     
