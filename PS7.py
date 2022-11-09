bf4dfaedb2de25993621532c44117e2ec11e36c8
iPupkin <blakeyvd@gmail.com>
2022-11-09 13:00:20 -0700

    fixed boruvka but safe edges does not work

--- PS7.py
+++ PS7.py
@@ -8,28 +8,21 @@ distance = lambda v1, v2 : math.sqrt((v1[0] - v2[0])**2 + (v1[1] - v2[1])**2)
 
 # gets all the edges in the given graph
 def get_edges(graph):
+    # (u, v, dist), ...
     edges = []
-    for u in range(len(graph)):
-        for v, dist in graph[u]:
-            if u < v: # don't waste time on other half of graph since it is undirected
-                edges.append((u, v, dist))
+    for u in graph:
+        for v in graph[u]:
+            if u < v[0]: # don't waste time on other half of graph since it is undirected
+                edges.append((u, v[0], v[1]))
                 
     return edges
 
-# add safe edge to a graph
-def add_safe_edges(F, safe):
-    for u, v, dist in safe:
-        if not (v, dist) in F[u]:
-            F[u].append((v, dist))
-        if not (u, dist) in F[v]:
-            F[v].append((u, dist))
-
 # gets all safe edges in graph
 def get_safe_edges(graph, comps):
-    total_comps = max(comps) + 1
+    total_comps = max(comps.values()) + 1
     
     # maintain a list of each comps of the best edge we find
-    #(u, v, distance)
+    # (u, v, distance), ...
     best_edge = [(-1, -1, sys.maxsize) for _ in range(total_comps)]
     
     # loop through all edges
@@ -44,37 +37,48 @@ def get_safe_edges(graph, comps):
        
     return best_edge
 
-# returns a list of components in graph
+# add safe edge to a graph
+def add_safe_edges(F, safe):
+    for u, v, dist in safe:
+        if not (v, dist) in F[u]:
+            F[u].append((v, dist))
+        if not (u, dist) in F[v]:
+            F[v].append((u, dist))
+            
+# returns a dict of components in graph => (x, y) : component #
 def find_components(graph):
-    comp = [None for _ in graph]
+    comp = dict()
     count = -1
+    
+    for u in graph: # empty comp 
+        comp[u] = None
     
     def dfs(u):
         if comp[u] == None:
             comp[u] = count
-            for v, _ in graph[u]:
-                dfs(v)
+            for v in graph[u]:
+                dfs(v[0]) # get (x, y) ignore dist
     
-    for u in range(len(graph)):
+    for u in graph:
         if comp[u] == None:
             count += 1
             dfs(u)
+    
     return comp
 
 # boruvkas method for MST
 def boruvka(graph):
     n = len(graph)
     
-    # F is non wieghted 
-    # F = ((V_index, x, y), ...)
-    F = [[] for _ in range(n)]
-    
+    # F = ((x,y) : (x, y), dist), ...)
+    F = dict()
+    for u in graph:
+        add_edge(F, u, set())
+
     # stop looping when F is a tree
-    while get_edges(F) < n - 1:
+    while len(get_edges(F)) < n - 1:
         comps = find_components(F)
-        
         safe = get_safe_edges(graph, comps)
-        
         add_safe_edges(F, safe)
         
     return F
@@ -99,13 +103,12 @@ def input():
     
     graph = dict() # create empty graph
     vertices = [] # keep track of ith vertice pos 
+    ignore = [] # edges to have 0 weight stored as ((x, y), (x2,y2))
     
     # set up graph from vertice inputs
     for i in range(n):
         x, y = list(map(float, sys.stdin.readline().split(' ')))
         key = ((x, y))
-        
-        add_edge(graph, key, set()) # empty graph for setup
         vertices.append(key)
     
     # get p input and add edges to graph
@@ -115,30 +118,49 @@ def input():
         # correct the indexing to start at 0
         u = vertices[a-1]
         v = vertices[b-1]
-        dist = 0
-        
-        add_edge(graph, u, (v, dist))  # key : (pos=(x, y), dist)
-        add_edge(graph, v, (u, dist))
+        ignore.append((u, v))
         
     # connect the first e values given in input
     if e > 1: # ignore base case, it will become its own single component anyway
         walkable = []
         for i in range(e):
-            walkable.append(vertices[e]) # add in order of input given
-        # connect them
-        # does not matter how they connect they all have weight 0
+            walkable.append(vertices[i]) # add in order of input given
+        # connect them to eachother, does not matter how they connect they all have weight 0
         for i in range(len(walkable) - 1):
             u = walkable[i]
             v = walkable[i+1] # 0 distance
             
-            add_edge(graph, u, (v, dist))
-            add_edge(graph, v, (u, dist))
-            
+            ignore.append((u, v))
+    
+    # connect all vertices to eachother
+    # only add distances that are short to graph to make it faster
+    for u in vertices:
+        for v  in vertices:
+            if u < v: # only go through half its undirected
+                if (u, v) in ignore: # give them 0 weight
+                    add_edge(graph, u, (v, 0))
+                    add_edge(graph, v, (u, 0))
+                else:
+                    dist = distance(u, v)
+                    add_edge(graph, u, (v, dist))
+                    add_edge(graph, v, (u, dist))
+           
     return graph
+
+# human readable graph
+def print_graph(G):
+    sum = 0
+    for u in G:
+        for v in G[u]:
+            print(u, ' connects to ', v[0], ' with w=', v[1])
+            sum += len(G[u])
+    print(sum)      
+    
 
 def main():
     G = input()
+    MST = boruvka(G)
     
-    return G
+    return 
 
 sys.stdout.write((str)(main()))
