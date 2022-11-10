e7d4d7deb8ba7d840c87b9047c2aeec7b6392862
iPupkin <blakeyvd@gmail.com>
2022-11-09 19:17:21 -0700

    optimizations

--- PS7.py
+++ PS7.py
@@ -1,11 +1,7 @@
 # Blake Van Dyken
 import sys
-sys.setrecursionlimit(10000)
-import math
-import cProfile
-
-# helper expression to get dist between two vertices
-distance = lambda v1, v2 : math.sqrt((v1[0] - v2[0])**2 + (v1[1] - v2[1])**2)
+from math import sqrt
+#import cProfile
 
 # gets all the edges in the given graph
 def get_edges(graph):
@@ -23,7 +19,7 @@ def get_safe_edges(graph, comps):
     total_comps = max(comps.values()) + 1
     # maintain a list of each comps of the best edge we find
     # (u, v, distance), ...
-    best_edge = [(None, None, sys.maxsize) for _ in range(total_comps)]
+    best_edge = [(-1, -1, sys.maxsize) for _ in range(total_comps)]
     
     # loop through all edges
     for u, v, dist in get_edges(graph):
@@ -72,7 +68,6 @@ def boruvka(graph):
     # stop looping when F is a tree
     while len(get_edges(F)) < (n - 1):
         comps = find_components(F)
-
         safe = get_safe_edges(graph, comps)
         # add our safe edges to F
         for u, v, dist in safe:
@@ -100,33 +95,26 @@ def add_edge(graph, k, v):
 # graph = (pos=(x, y)) : (pos=(x, y), dist)
 def input():
     # read first line of input
-    n, e, p = list(map(int, sys.stdin.readline().split(' ')))
-    
+    n, e, p = sys.stdin.readline().split(' ')
     graph = dict() # create empty graph
     vertices = [] # keep track of ith vertice pos 
     ignore = [] # edges to have 0 weight stored as ((x, y), (x2,y2))
     
     # set up graph from vertice inputs
-    for i in range(n):
-        x, y = list(map(float, sys.stdin.readline().split(' ')))
-        key = ((x, y))
-        vertices.append(key)
+    for i in range(int(n)):
+        x, y = sys.stdin.readline().split(' ')
+        vertices.append((float(x), float(y)))
     
     # get p input and add edges to graph
-    for i in range(p):
-        a, b = list(map(int, sys.stdin.readline().split(' ')))
-        
+    for i in range(int(p)):
+        a, b = sys.stdin.readline().split(' ') 
         # correct the indexing to start at 0
-        u = vertices[a-1]
-        v = vertices[b-1]
-        ignore.append((u, v))
+        ignore.append((vertices[int(a)-1], vertices[int(b)-1]))
   
     # connect the first e values given in input
-    if e > 1: # ignore base case, it will become its own single component anyway
-        for i in range(e-1):
-            u = vertices[i]
-            v = vertices[i+1]
-            ignore.append((u, v))
+    if int(e) > 1: # ignore base case, it will become its own single component anyway
+        for i in range(int(e)-1):
+            ignore.append((vertices[i], vertices[i+1]))
 
     # only add distances that are short to graph to make it faster
     # create a connected graph by connecting all to first vertice
@@ -137,7 +125,7 @@ def input():
                     add_edge(graph, u, (v, 0))
                     add_edge(graph, v, (u, 0))
                 else:
-                    dist = distance(u, v)
+                    dist = sqrt((u[0] - v[0])**2 + (u[1] - v[1])**2)
                     add_edge(graph, u, (v, dist))
                     add_edge(graph, v, (u, dist))
            
@@ -149,7 +137,7 @@ def main():
     rope = 0
     
     for s in MST.values():
-        if len(s) > 0:
+        if s != set():
             for edge in s:
                 rope += edge[1]
 
