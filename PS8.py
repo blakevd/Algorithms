19e5106ea9f54bfa65f75730b391824e3cd9fe86
iPupkin <blakeyvd@gmail.com>
2022-11-14 17:20:05 -0700

    almost working dijkstra

--- PS8.py
+++ PS8.py
@@ -12,39 +12,35 @@ def add_edge(graph, k, v):
         except KeyError:
             graph[k] = {v}
 
-def initSSSP(graph, size):
-    dist = [float('inf') for _ in range(size)]
-    pred = [None for _ in range(size)]
-
-    dist[0] = 0
-    return dist, pred
-
-
 # change all edge weights to be negative and then we will find longest path
 # follows non negative dijkstras structure
-def dijkstra(graph, size, start, end):
+def dijkstra(graph, size, start):
     q = PriorityQueue()
-    dist, pred = initSSSP(graph, size)
-    print('init dist = ', dist)
+    dist = [-float('inf') for _ in range(size)] # initSSSP
+    
     # load priority queue
     for v in graph:
         q.put( (v, dist[v]) )
 
     while not q.empty():
-        next = q.get() # get next min item in pq
+        # get next minheap item in pq
+        next = q.get()
         u = next[0]
-        print('dist = ', next)
+        u_dist = next[1] 
+        print('u', u)
         for edge in graph[u]:
                 v = edge[0]
                 uv_dist = edge[1]
-                if pred[v] != u:
-                    # tense => dist(u) + w(u->v) < dist(v)
-                    tense = dist[u] + uv_dist
-                    print('u:',dist[u], 'v:',uv_dist)
-                    if dist[v] > tense: # if it is tense
-                        dist[v] = tense # relax it
-                        pred[v] = u
-                        q.put( (v, dist[v]) )
+                
+                # tense => dist(u) + w(u->v) < dist(v) 
+                tense = u_dist * uv_dist       
+                if u == start: # we are at first edge so don't multiply by 0
+                    tense = uv_dist
+                print('check ', tense, dist[v])
+                if tense > dist[v]: # check if tense
+                    dist[v] = tense # relax
+                    print('adding', (v, dist[v]))
+                    q.put( (v, dist[v]) )
     
     return dist
 
@@ -53,24 +49,28 @@ def input():
     n, m = stdin.readline().split(' ')
     graph = dict()
 
+    for i in range( int(m) ):
+        add_edge(graph, i, set())
+    
     for _ in range( int(m) ):
         # 2<=x<=10000
         # 1<=y<=15000
         x, y, f = stdin.readline().split(' ')
+        
+        # make weight negative for dijktras longest path
         key = int(x)
         value = int(y)
-        # 0 <= f <= 1
-        weight = float(f) # make weight negative for dijktras longest path
+        weight = float(f) # 0 <= f <= 1
+        
         # add undirected edge to adjacency list
         add_edge(graph, key, (value, weight))
-        add_edge(graph, value, (key, weight))
 
     return graph, int(m), int(n)
 
 def main():
     G, m, n = input()
-    dist = dijkstra(G, m, 0, n-1)
-
+    dist = dijkstra(G, m, 0)
+    
     return dist
 
 stdout.write(str(main()))
