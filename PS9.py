5ac7dc5c1340a1e87cef32fa0523d722d31d02c3
iPupkin <blakeyvd@gmail.com>
2022-11-20 18:37:57 -0700

    2D array works

--- PS9.py
+++ PS9.py
@@ -6,7 +6,7 @@ from math import sqrt
 def input():
     # vertices
     n = int(stdin.readline())
-    graph = [[None for col in range(n)] for row in range(n)] # 2D array for storage of position VxV
+    graph = [[float('inf') for col in range(n)] for row in range(n)] # 2D array for storage of position VxV
     pos = []
     for i in range(n):
         x, y = stdin.readline().split(' ')
@@ -23,7 +23,23 @@ def input():
         b = int(b)
         
         # append distance to 2D array
-        graph[a][b] = (sqrt( (pos[b][0] - pos[a][0])**2 + (pos[b][1] - pos[a][1])**2 ))
+        dist = sqrt( (pos[b][0] - pos[a][0])**2 + (pos[b][1] - pos[a][1])**2 )
+        graph[a][b] = (dist)
+        graph[b][a] = (dist)
+    
+    # add another road that is the shortest
+    min = float('inf')
+    x, y = -1, -1
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                if graph[i][j] == float('inf') or graph[i][j] == float('inf'): # not already a path we have made
+                    dist = sqrt( (pos[i][0] - pos[j][0])**2 + (pos[i][1] - pos[j][1])**2 )
+                    if dist < min:
+                        min = dist
+                        x, y = i, j
+    
+    graph[x][y] = min
     return graph
 
 def main():
